ce1d0f73504d5c446e121cfeb53a8b3bec7a7288
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-17 21:14:36 +0600

    Add files via upload

---
+++ Foreachloop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreachloop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* 
+             foreach (value in collection)
+             {
+              statement;
+             }
+
+             */
+            string[] arr = new string[5];
+            arr[0] = "jannatul Ferdous Munia";
+            arr[1] = "age";
+            arr[2] = "education";
+            arr[3] = "religion";
+            arr[4] = "family";
+            foreach(string name in arr)
+            {
+                Console.WriteLine("Details:" + name);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
