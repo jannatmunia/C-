67fd8210f7cd9086b1f7c3a0f7dc9eb76e85061a
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-17 21:31:23 +0600

    Add files via upload

---
+++ ForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             for(initialization;condition;increment/decrement)
+             {
+                statements;
+             }
+             */
+            for (int z = 20; z > 0; z--)
+            {
+                Console.Write("Values are : {0}\n", z);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
