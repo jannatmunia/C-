663d76a724e4124db500875f13e2a6d716ada248
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-18 16:06:07 +0600

    Add files via upload

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            /*
+             while(condition)
+             {
+                statements;
+             }
+             */
+            while (count < x)
+            {
+                Console.Write("The value is {0} \n", x);
+                if (x == 4)
+                    break;
+                x--;
+
+            }
+            Console.ReadKey();
+        }
+    
+    }
+}
