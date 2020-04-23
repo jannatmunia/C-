1e6ef8bfec25714ca2f078a6cd62bb23c6673a21
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-24 00:25:09 +0600

    Add files via upload

---
+++ MultipleTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace MultipleTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Print Multiplication Table 
+             */
+            int factor1, factor2;
+            Console.WriteLine("Enter a number:");
+            factor1 = Convert.ToInt32(Console.ReadLine());
+
+            for(factor2 = 1; factor2 <= 25; factor2++)
+            {
+                Console.WriteLine("{0}*{1} = {2}",factor1,factor2,(factor1*factor2));
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
