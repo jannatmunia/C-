bfb65926f594cabdb43da151f873c761134a7be5
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-25 22:08:30 +0600

    Add files via upload

---
+++ IfElse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y;
+            Console.WriteLine("Enter a number:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+            else
+            {
+                Console.WriteLine("y is less than x");
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+
