e79a8ad49bf165f3154fd9bc097613af22136d6b
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-27 23:51:39 +0600

    Add files via upload

---
+++ DivisorsOfNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DivisorsOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a natural number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The divisors for the number {0} are ", number);
+
+            for(int divisor = 1;divisor<= number; divisor++)
+            {
+                if((number%divisor)==0)
+                {
+                    Console.WriteLine("{0}", divisor);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
