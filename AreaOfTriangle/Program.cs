73b9ded7da4e4905010d2d25b826c39b22164a68
jannatmunia <43574672+jannatmunia@users.noreply.github.com>
2020-04-27 22:59:39 +0600

    Add files via upload

---
+++ AreaOfTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area,b,h;
+            Console.WriteLine("Enter the base of triangle:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the height of triangle:");
+            h = Convert.ToDouble(Console.ReadLine());
+            /* Formula of a Triangle
+            Area = (b*h)/2  where b is the base and h is the height of the triangle.
+            */
+            area = (b * h) / 2;
+            Console.WriteLine("Area of triangle with base and height is {0},{1} and area is {2}", b, h, area);
+            Console.ReadKey();
+        }
+    }
+}
